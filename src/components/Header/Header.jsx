06e6e43ecda1d78fc69f6596f7be0fea4abdd4c1
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-09-27 18:22:18 -0300

    Added logout button to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,30 @@ import styles from './Header.module.css';
 
 import { Link } from 'react-router-dom';
 
+import { useAuthContext } from '../AuthContext/AuthContext.jsx';
+
 function Header() {
+    const authContext = useAuthContext();
+
     return (
         <header className={styles.header}>
             <Link to={"/"} className={styles.headerLink}>
                 <img src="/chameca.png" alt="chameca" draggable="false" />
                 <h1>Rascunho</h1>
             </Link>
+
+            {
+                (authContext.loggedUsername !== null) &&
+                <div className={styles.userInfo}>
+                    <p>{authContext.loggedUsername}</p>
+                    <button
+                        className={styles.logoutButton}
+                        onClick={authContext.logout}
+                    >
+                        <img src="/src/imgs/exit.png" alt="" />
+                    </button>
+                </div>
+            }
         </header>
     );
 }
