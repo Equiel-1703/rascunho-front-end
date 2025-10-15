1d042d733a2b028650d20663b71106092cbd2a23
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-10-15 00:54:03 -0300

    Refactor Home component layout and styles for improved structure and consistency

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -30,74 +30,74 @@ function Home() {
         );
     }
 
+    if (authContext.loggedUsername !== null) {
+        return (
+            <main className={styles.mainNotebook}>
+                <Notebook />
+            </main>
+        );
+    }
+
     return (
         <main className={styles.main}>
-            {
-                (authContext.loggedUsername !== null) ? (
-                    <Notebook />
-                ) : (
-                    <>
-                        <section className={styles.hero}>
-                            <img src="/ss_1.png" alt="Print 1" />
-                            <div className={styles.heroText}>
-                                <h2>Os seus pensamentos merecem mais que uma mensagem no WhatsApp!</h2>
-                                <p>
-                                    Escreva suas ideias, notas e rascunhos  — tudo em um único e simples lugar.
-                                </p>
-                                <button
-                                    className={styles.homeButton}
-                                    onClick={() => navigate('/register')}
-                                >
-                                    Comece a Escrever!
-                                </button>
-                            </div>
-                        </section>
+            <section className={styles.hero}>
+                <img src="/ss_1.png" alt="Print 1" />
+                <div className={styles.heroText}>
+                    <h2>Os seus pensamentos merecem mais que uma mensagem no WhatsApp!</h2>
+                    <p>
+                        Escreva suas ideias, notas e rascunhos  — tudo em um único e simples lugar.
+                    </p>
+                    <button
+                        className={styles.homeButton}
+                        onClick={() => navigate('/register')}
+                    >
+                        Comece a Escrever!
+                    </button>
+                </div>
+            </section>
 
-                        <section className={styles.why}>
-                            <div>
-                                <h2>Por que o Rascunho?</h2>
-                                <p>
-                                    A maioria das pessoas costuma usar o WhatsApp para escrever pequenos textos,
-                                    como listas de compras, lembretes e rascunhos rápidos. No entanto, isso pode se
-                                    perder no meio de outras mensagens, tornando difícil encontrar essas informações depois.
-                                </p>
-                                <p className={styles.lastLine}>
-                                    O Rascunho oferece um espaço limpo, organizado e rápido para as suas ideias!
-                                </p>
-                            </div>
-                            <img src="/wpp.png" alt="Screenshot do WhatsApp cheio de anotações - coisa do passado!" />
-                        </section>
+            <section className={styles.why}>
+                <div>
+                    <h2>Por que o Rascunho?</h2>
+                    <p>
+                        A maioria das pessoas costuma usar o WhatsApp para escrever pequenos textos,
+                        como listas de compras, lembretes e rascunhos rápidos. No entanto, isso pode se
+                        perder no meio de outras mensagens, tornando difícil encontrar essas informações depois.
+                    </p>
+                    <p className={styles.lastLine}>
+                        O Rascunho oferece um espaço limpo, organizado e rápido para as suas ideias!
+                    </p>
+                </div>
+                <img src="/wpp.png" alt="Screenshot do WhatsApp cheio de anotações - coisa do passado!" />
+            </section>
 
-                        <section className={styles.how}>
-                            <h2>Como funciona?</h2>
-                            <div className={styles.steps}>
-                                <div className={styles.step}>
-                                    <h3>🗒️ Escreva</h3>
-                                    <p>Crie uma nota com título e texto</p>
-                                </div>
-                                <div className={styles.step}>
-                                    <h3>🏷️ Tag</h3>
-                                    <p>Adicione suas próprias tags para organizar suas anotações.</p>
-                                </div>
-                                <div className={styles.step}>
-                                    <h3>🔍 Busque</h3>
-                                    <p>Encontre facilmente suas notas filtrando pelas tags que você criou. É só clicar nelas!</p>
-                                </div>
-                            </div>
-                        </section>
+            <section className={styles.how}>
+                <h2>Como funciona?</h2>
+                <div className={styles.steps}>
+                    <div className={styles.step}>
+                        <h3>🗒️ Escreva</h3>
+                        <p>Crie uma nota com título e texto</p>
+                    </div>
+                    <div className={styles.step}>
+                        <h3>🏷️ Tag</h3>
+                        <p>Adicione suas próprias tags para organizar suas anotações.</p>
+                    </div>
+                    <div className={styles.step}>
+                        <h3>🔍 Busque</h3>
+                        <p>Encontre facilmente suas notas filtrando pelas tags que você criou. É só clicar nelas!</p>
+                    </div>
+                </div>
+            </section>
 
-                        <section className={styles.cta}>
-                            <h2>Pronto para começar?</h2>
-                            <p>
-                                Sem e-mail, sem dados pessoais e sem blá-blá-blá. Crie um nome de usuário, uma senha e comece a escrever!
-                            </p>
-                            <button className={styles.homeButton} onClick={() => navigate('/register')}>
-                                Crie sua conta
-                            </button>
-                        </section>
-                    </>
-                )
-            }
+            <section className={styles.cta}>
+                <h2>Pronto para começar?</h2>
+                <p>
+                    Sem e-mail, sem dados pessoais e sem blá-blá-blá. Crie um nome de usuário, uma senha e comece a escrever!
+                </p>
+                <button className={styles.homeButton} onClick={() => navigate('/register')}>
+                    Crie sua conta
+                </button>
+            </section>
         </main>
     );
 }
